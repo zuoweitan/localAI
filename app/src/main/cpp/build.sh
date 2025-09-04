a84f9fdea0b44a70914035bbab006f27c726255e
zhigui.tww <zhigui.tww@alibaba-inc.com>
2025-09-04 10:43:06 +0800

    【Demand】解决apple m3编译问题

--- app/src/main/cpp/build.sh
+++ app/src/main/cpp/build.sh
@@ -1,6 +1,11 @@
 set -e
-cmake --preset android-release
-cmake --build --preset android-release
+
+# 切换到脚本所在目录
+cd "$(dirname "$0")"
+
+cmake --preset android-release -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+# 添加-j2选项限制并行编译以解决内存不足问题
+cmake --build --preset android-release -- -j2
 
 mkdir -p lib
 cp ./build/android/qnnlibs/*.so ./lib
